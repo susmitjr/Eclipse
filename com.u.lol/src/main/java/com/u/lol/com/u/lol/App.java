9dd0b7342de8be6c4087ad50950dec478f5ccc06
SusmitJana <SusmitJana@DESKTOP-6ICKLCA>
2020-01-22 23:42:28 +0530

    Commiting

---
+++ com.u.lol/src/main/java/com/u/lol/com/u/lol/App.java
@@ -0,0 +1,13 @@
+package com.u.lol.com.u.lol;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
